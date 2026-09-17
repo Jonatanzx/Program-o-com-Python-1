b189583b039f33139515fd4735c5cb6fa3b9d61a
Jonathan gay <eu.jonatan777@gmail.com>
2026-09-17 10:29:55 -0300

    Add files via upload

---
+++ Aula22 Funções 2/main (1).py
@@ -0,0 +1,8 @@
+# def apresentar(nome, idade):
+#     print("Olá", nome, "Sua idade é", idade)
+
+def media(n1, n2, n3):
+    media_aritimetica = n1 + n2 + n3 / 3
+    return media_aritimetica
+
+print(media(10, 7, 8))
